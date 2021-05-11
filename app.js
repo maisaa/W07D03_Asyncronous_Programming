8e1b8f89e3bf7437be83ba1cb272f3e0fdd56c35
maisaa <maisaa.alkhedr@gmail.com>
2021-05-11 12:07:44 +0200

    read file

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 const axios = require("axios");
 
-const readFile = () =>{
-    
-}
+
+const readFile = () => {
+    let content;
+    fs.readFile("./data.txt", (err, data) => {
+        if (err) throw err;
+        content = data.toString();
+        console.log("content :", content);
+    })
+};
+
+readFile();
